1d7a0b55a56f51000bd830b07103a6a9337d2706
j2766 <jjosemanuel64@gmail.com>
2020-04-18 08:50:53 -0700

    moved files into a class

--- rename.py
+++ rename.py
@@ -2,43 +2,45 @@ import os
 import pathlib
 
 
+class Rename:
+	def __init__(self,directory):
+		self.self.directory=self.directory
+
+	def find_all_files_in_select_self.directory(self):
+		return  [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
 
 
-def find_all_files_in_select_directory(directory):
-	return  [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+	def find_all_files_of_type_in_select_self.directory( self, file_type):
+	        files = []
+	        try:
+	            for f in os.listdir(self.directory):
+	                if pathlib.Path(f).suffix == filetype:
+	                    files.append(f)
+	        except Exception as e:
+	            print(e)
+	        return files
 
 
-def find_all_files_of_type_in_select_directory( directory, file_type):
-        files = []
-        try:
-            for f in os.listdir(directory):
-                if pathlib.Path(f).suffix == filetype:
-                    files.append(f)
-        except Exception as e:
-            print(e)
-        return files
+	def rename_files_as_sequence(self):
+		files=find_all_files_in_select_self.directory(self.directory)
+		i=1
+		for file in files:
 
 
-def rename_files_as_sequence(directory):
-	files=find_all_files_in_select_directory(directory)
-	i=1
-	for file in files:
+			full_name=self.directory+'\\'+file
+			splitted_file=os.path.splitext(file)
+
+			os.rename(full_name,self.directory+'\\'+str(i)+splitted_file[1])
+			i+=1
 
 
-		full_name=directory+'\\'+file
-		splitted_file=os.path.splitext(file)
-
-		os.rename(full_name,directory+'\\'+str(i)+splitted_file[1])
-		i+=1
-
-
-def rename_files_relative_to_base_file(directory):
-	basename=os.path.basename(directory)
-	files=find_all_files_in_select_directory(directory)
-	i=1
-	for file in files:
-		full_name=directory+'\\'+file
-		splitted_file=os.path.splitext(file)
-		os.rename(full_name,directory+'\\'+basename+'_'+str(i)+splitted_file[1])
-		i+=1
+	def rename_files_relative_to_base_file(self):
+		basename=os.path.basename(self.directory)
+		files=find_all_files_in_select_self.directory(self.directory)
+		i=1
+		for file in files:
+			full_name=self.directory+'\\'+file
+			splitted_file=os.path.splitext(file)
+			os.rename(full_name,self.directory+'\\'+basename+'_'+str(i)+splitted_file[1])
+			i+=1
 
